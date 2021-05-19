2278664c86a7d86c83ebacff2d2c0964f640f1bf
Caleb Salyards <34790708+Psymatics@users.noreply.github.com>
2021-05-19 11:15:56 -0600

    Create director.py

---
+++ jumper/game/director.py
@@ -0,0 +1,10 @@
+from game.parachute import Parachute
+from game.word import Word
+from game.console import Console
+
+class Director:
+
+    def __init__(self)
+        parachute = Parachute()
+        word = Word()
+        console = Console()
