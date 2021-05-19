65f6b096322a8770d000dab96d8ea35524c1bf86
Caleb Salyards <34790708+Psymatics@users.noreply.github.com>
2021-05-19 12:56:18 -0600

    Update director.py

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -4,7 +4,23 @@ from game.console import Console
 
 class Director:
 
-    def __init__(self)
-        parachute = Parachute()
-        word = Word()
-        console = Console()
+    def __init__(self):
+        self.parachute = Parachute()
+        self.word = Word()
+        self.console = Console()
+        self.keep_playing = True
+
+    def start_game(self):
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs():
+        pass
+
+    def do_updates():
+        pass
+
+    def do_outputs():
+        pass
