c9b5d7fee21f365de9a7ba91b880b839c9290aee
pondels <yeckron@comcast.net>
2021-05-19 11:02:27 -0600

    Letter Class and Words.csv
    Added the functionality of the random word generator and created a csv file to pull from.

---
+++ jumper/game/letter.py
@@ -0,0 +1,16 @@
+import random
+
+class Letter:
+    def __init__(self):
+        randomized_word = self.random_word()
+
+    def random_word(self):
+        with open('jumper\game\words.csv', 'rt') as file:
+            empty_array = []
+            for i in file:
+                empty_array.append(i)
+            random_word = random.choice(empty_array)
+            random_word = random_word.strip('\n')
+            return random_word
+
+Letter()
