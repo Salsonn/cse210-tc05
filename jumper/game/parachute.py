3e03047b5988da4a6d4ce2205a8d19322b1d19eb
Josh Thieme <jomthieme@gmail.com>
2021-05-20 10:13:10 -0600

    Updated Parachute and deleted letter.py

--- jumper/game/parachute.py
+++ jumper/game/parachute.py
@@ -1,22 +1,25 @@
-from game.letter import Letter
+from game.word import Word
 
 class Parachute:
     def __init__(self):
-        self.letter = Letter()
+        self.word = Word()
 
     def create_graphics(self):
         sky = '__ __ __ __ __ \n\n'
-        parachute_layer1 = ' ______\n'
-        parachute_layer2 = '/\t\\\n'
-        parachute_layer3 = ' ____\n'
-        parachute_layer4 = '\\   /\n'
-        parachute_layer5 = '\\  /\n'
-        parachute_layer6 = '   0  \n'
-        parachute_layer7 = '/ | \\\n'
-        parachute_layer8 = '/  \\\n\n'
+        parachute = [
+            [' ___'],
+            ['/___\\'],
+            ['\\   /'],
+            ['\\ /']
+            ]
+        person = [
+            [' 0'],
+            ['/|\\'],
+            ['/ \\']
+        ] 
         ground = '^^^^^^^^^^^^'
 
-    def change_layer6(self):
+    def change_layer6(self, person):
         if self.director.game_over:
-            parachute_layer6 = '   X  \n'
-            return parachute_layer6
+            person[[0]] = '   X  \n'
+            return person[[0]]
